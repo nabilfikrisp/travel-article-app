aca2d476c2493634e2e5a841af4c65a5e358536e
nabilfikrisp <mnabilfikrisp@gmail.com>
2025-01-03 22:32:56 +0700

    prettier and tailwind prettier init

--- src/app/index/page.tsx
+++ src/app/index/page.tsx
@@ -1,5 +1,10 @@
 import { Button } from "@/components/ui/button";
 
 export default function MainPage() {
-  return <Button>hallo</Button>;
+  return (
+    <div>
+      <h1 className="mt-0 text-sm font-bold">INI PAGE UTAMA</h1>
+      <Button>hallo</Button>
+    </div>
+  );
 }
